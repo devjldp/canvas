7a436bee5bd5d3848ceb668db065e0d29c67d5c2
devjldp <joseluisdp297@gmail.com>
2023-07-19 23:12:39 +0100

    coorecion

--- js/canvas.js
+++ js/canvas.js
@@ -1,17 +1,12 @@
 let miCanva = document.getElementById('miCanvas');
 let ctx = miCanva.getContext('2d');
 
+
 window.addEventListener('DOMContentLoaded', () =>{
-
-
   console.log(ctx)
-
   let indy = new Image();
   indy.src = '../images/indy.png';
   indy.addEventListener('load', ()=>{
     ctx.drawImage(indy,60,60,100,100)
   })
-  
-  
-
 })
