e0b5b70f5c81c08d102a26a99dcbf84498810305
devjldp <joseluisdp297@gmail.com>
2023-07-19 23:18:04 +0100

    correcion2

--- js/canvas.js
+++ js/canvas.js
@@ -2,8 +2,6 @@ window.addEventListener('DOMContentLoaded', () =>{
 
   let miCanva = document.getElementById('miCanvas');
   let ctx = miCanva.getContext('2d');
-
-  console.log(ctx)
   let indy = new Image();
   indy.src = '../images/indy.png';
   indy.addEventListener('load', ()=>{
