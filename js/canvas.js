808ff2decd7d743c0ee1952a33ad881c59f48521
devjldp <joseluisdp297@gmail.com>
2023-07-19 23:05:50 +0100

    add imagen

--- js/canvas.js
+++ js/canvas.js
@@ -1,7 +1,13 @@
 window.addEventListener('DOMContentLoaded', () =>{
 
-  let miCanva = document.getElementById('miCanvas');
-  let ctx = miCanva.getContext('2d');
   console.log(ctx)
+
+  let indy = new Image();
+  indy.src = '../images/indy.png';
+  indy.addEventListener('load', ()=>{
+    ctx.drawImage(indy,60,60,100,100)
+  })
+  
+  
 
 })
