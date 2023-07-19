005af5a60cbd02e9fbe5cd89aef754ee9f80034e
JoseDev <132334488+devjldp@users.noreply.github.com>
2023-07-19 23:20:30 +0100

    Update canvas.js

--- js/canvas.js
+++ js/canvas.js
@@ -3,7 +3,7 @@ window.addEventListener('DOMContentLoaded', () =>{
   let miCanva = document.getElementById('miCanvas');
   let ctx = miCanva.getContext('2d');
   let indy = new Image();
-  indy.src = '../images/indy.png';
+  indy.src = 'https://devjldp.github.io/images/indy.png';
   indy.addEventListener('load', ()=>{
     ctx.drawImage(indy,60,60,100,100)
   })
